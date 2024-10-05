cdd733847f0a4dd66205ded9f08e1ca0544f92bd
Jorge Alvarez <jorgehaq@gmail.com>
2024-10-05 10:10:11 -0500

    components and module

--- mono-workspace/projects/mfe-app/src/app/app-routing.module.ts
+++ mono-workspace/projects/mfe-app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/todo-list', pathMatch:'full'},
+  {path:'todo-list', component:TodoListComponent, pathMatch:'full'},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
